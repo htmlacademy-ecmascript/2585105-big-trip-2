01fbc99fc2c870d464b86012b47a52f2e04e8669
Анна Крамер <anna.pro.kramer@gmail.com>
2025-01-22 15:36:00 +0300

    добавляет запятые

--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -1,9 +1,8 @@
-import SortView from '../view/sort-view.js';
-import EditList from '../view/event-list-view.js'
-import FormEditView from '../view/form-edit-view.js'
-import PointView from '../view/point-view.js'
-import { render } from '../render.js';
-
+import SortView from "../view/sort-view.js";
+import EditList from "../view/event-list-view.js";
+import FormEditView from "../view/form-edit-view.js";
+import PointView from "../view/point-view.js";
+import { render } from "../render.js";
 
 export default class BoardPresenter {
   sortComponent = new SortView();
@@ -19,7 +18,7 @@ export default class BoardPresenter {
     render(new FormEditView(), this.editListComponent.getElement());
 
     for (let i = 0; i < 3; i++) {
-      render(new PointView, this.editListComponent.getElement())
+      render(new PointView(), this.editListComponent.getElement());
     }
   }
 }
--- src/view/point-view.js
+++ src/view/point-view.js
@@ -59,3 +59,4 @@ export default class PointView {
     this.element = null;
   }
 }
+
