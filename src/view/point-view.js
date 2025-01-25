eb4d00f791215d8c74c67880632b205f94ac9f5d
Анна Крамер <anna.pro.kramer@gmail.com>
2025-01-25 14:41:00 +0300

    настраивает представление точки маршрута так, чтобы контент ожидался из внешнего источника.

--- src/view/point-view.js
+++ src/view/point-view.js
@@ -1,34 +1,43 @@
 import { createElement } from '../render.js';
+import { formatStringToShortDate, formatStringToDayTime, formatStringToTime, getPointDuration } from '../utils.js';
 
-function createPointTemplate() {
+function createPointTemplate({ point, pointDestinations, pointOffers }) {
+  const { basePrice, dateFrom, dateTo, offers, isFavorite, type } = point;
+
   return `
         <li class="trip-events__item">
             <div class="event">
-            <time class="event__date" datetime="2019-03-18">MAR 18</time>
+            <time class="event__date" datetime=${formatStringToDayTime(dateFrom)}>
+              ${formatStringToShortDate(dateFrom)}
+            </time>
             <div class="event__type">
-                <img class="event__type-icon" width="42" height="42" src="img/icons/taxi.png" alt="Event type icon">
+                <img class="event__type-icon" width="42" height="42" src="img/icons/${type}.png" alt="Event type icon">
             </div>
-            <h3 class="event__title">Taxi Amsterdam</h3>
+            <h3 class="event__title">${type} ${pointDestinations.name}</h3>
             <div class="event__schedule">
                 <p class="event__time">
-                <time class="event__start-time" datetime="2019-03-18T10:30">10:30</time>
+                <time class="event__start-time" datetime=${formatStringToDayTime(dateFrom)}>${formatStringToTime(dateFrom)}</time>
                 &mdash;
-                <time class="event__end-time" datetime="2019-03-18T11:00">11:00</time>
+                <time class="event__end-time" datetime=${formatStringToDayTime(dateTo)}>${formatStringToTime(dateTo)}
                 </p>
-                <p class="event__duration">30M</p>
+                <p class="event__duration">${getPointDuration(dateFrom, dateTo)}</p>
             </div>
             <p class="event__price">
-                &euro;&nbsp;<span class="event__price-value">20</span>
+                &euro;&nbsp;<span class="event__price-value">
+                  ${basePrice}
+                </span>
             </p>
             <h4 class="visually-hidden">Offers:</h4>
             <ul class="event__selected-offers">
-                <li class="event__offer">
-                <span class="event__offer-title">Order Uber</span>
-                &plus;&euro;&nbsp;
-                <span class="event__offer-price">20</span>
-                </li>
+            ${pointOffers.map((offer) => `
+              <li class="event__offer">
+                <span class="event__offer-title">${offer.title}</span><br>
+                +€&nbsp;
+                <span class="event__offer-price">${offer.price}</span>
+              </li>`).join('')}
+
             </ul>
-            <button class="event__favorite-btn event__favorite-btn--active" type="button">
+            <button class="event__favorite-btn  ${(point.isFavorite) ? 'event__favorite-btn--active' : ''}" type="button">
                 <span class="visually-hidden">Add to favorite</span>
                 <svg class="event__favorite-icon" width="28" height="28" viewBox="0 0 28 28">
                 <path d="M14 21l-8.22899 4.3262 1.57159-9.1631L.685209 9.67376 9.8855 8.33688 14 0l4.1145 8.33688 9.2003 1.33688-6.6574 6.48934 1.5716 9.1631L14 21z"/>
@@ -43,8 +52,18 @@ function createPointTemplate() {
 }
 
 export default class PointView {
+  constructor({ point, pointDestinations, pointOffers }) {
+    this.point = point;
+    this.pointDestinations = pointDestinations;
+    this.pointOffers = pointOffers;
+  }
+
   getTemplate() {
-    return createPointTemplate();
+    return createPointTemplate({
+      point: this.point,
+      pointDestinations: this.pointDestinations,
+      pointOffers: this.pointOffers,
+    });
   }
 
   getElement() {
@@ -59,4 +78,3 @@ export default class PointView {
     this.element = null;
   }
 }
-
