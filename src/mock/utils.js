bb519bb6d20455e118e90b9e85c4c428a33c8e5f
Анна Крамер <anna.pro.kramer@gmail.com>
2025-01-31 16:14:39 +0300

    вносит правки в линтер

--- src/mock/utils.js
+++ src/mock/utils.js
@@ -2,12 +2,12 @@ import dayjs from 'dayjs';
 import { getRandomPositiveInteger } from '../utils';
 import { DURATION } from '../const';
 
-let date = dayjs().subtract(getRandomPositiveInteger(0, DURATION.DAY), 'day').toDate();
+let date = dayjs().subtract(getRandomPositiveInteger(0, DURATION.day), 'day').toDate();
 
 function getDate({ next }) {
-  const minsGap = getRandomPositiveInteger(0, DURATION.MIN);
-  const hoursGap = getRandomPositiveInteger(0, DURATION.HOUR);
-  const daysGap = getRandomPositiveInteger(0, DURATION.DAY);
+  const minsGap = getRandomPositiveInteger(0, DURATION.min);
+  const hoursGap = getRandomPositiveInteger(0, DURATION.hour);
+  const daysGap = getRandomPositiveInteger(0, DURATION.day);
 
   if (next) {
     date = dayjs(date)
