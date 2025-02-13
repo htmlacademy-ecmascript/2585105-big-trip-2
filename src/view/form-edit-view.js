711c976551ccef55727ee9818ffa67ef710d6109
Анна Крамер <anna.pro.kramer@gmail.com>
2025-02-13 14:53:25 +0300

    исправляет цену

--- src/view/form-edit-view.js
+++ src/view/form-edit-view.js
@@ -225,7 +225,7 @@ export default class FormEditView extends AbstractStatefulView {
     this._setState({
       point: {
         ...this._state.point,
-        basePrice: evt.target.valueAsNumber
+        basePrice: evt.target.value
       }
     });
   };
